4f46986af9641960c64ae4d8817be215af699533
KAITO <kaitoshibao30610614@gmail.com>
2025-11-08 23:40:55 +0900

    Clean up middleware.js by removing unused code
    Removed commented-out middleware functions for clarity.

--- middleware.js
+++ middleware.js
@@ -1,18 +1,3 @@
-
-// module.exports.storeReturnTo = (req, res, next) => {
-//     if (req.session.returnTo) {
-//         res.locals.returnTo = req.session.returnTo;
-//     }
-//     next();
-// }
-// module.exports.isLoggedIn = (req, res, next) => {
-//     if (!req.isAuthenticated()) {
-//         req.session.returnTo = req.originalUrl; // この行を追加
-//         req.flash('error', 'ログインしてください');
-//         return res.redirect('/login');
-//     }
-//     next();
-// }
 const ExpressError = require('./utils/ExpressError');
 const { campgroundSchema, reviewSchema} = require('./schemas');
 const Campground = require('./models/campground');
